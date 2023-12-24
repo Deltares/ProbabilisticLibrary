f5346e126ba45863ebfb3f99262dd91d1cbb7b77
Edwin Spee <edwin.spee@deltares.nl>
2023-12-24 15:44:14 +0100

    use rnd from dotnet ; now identical results

--- wrappers/Deltares.Reliability.Wrapper/Deltares.Reliability.Wrapper.cpp
+++ wrappers/Deltares.Reliability.Wrapper/Deltares.Reliability.Wrapper.cpp
@@ -4,12 +4,22 @@
 #include "../../src/probFuncs/stochSettings.h"
 #include "../../src/distributions/parseDistribution.h"
 #include "../../src/correlation/identity.h"
-#include "../../src/rnd/defaultRandomDouble.h"
 #include "../../src/probMethods/progress.h"
 
 namespace Deltares {
     namespace Reliability {
         namespace Wrapper {
+
+            void rndDotNet::getRandomVector(vector1D& r)
+            {
+                auto nrStoch = mr->VaryingStochastCount;
+                auto s = mr->GetRandomSample(nrStoch);
+                for (size_t i = 0; i < nrStoch; i++)
+                {
+                    r(i) = s->Values[i];
+                }
+            }
+
             double zmodelDotNet::zfunc(const probDraw& d) const
             {
                 auto nrStoch = mr->VaryingStochastCount;
@@ -27,9 +37,10 @@ namespace Deltares {
             {
                 auto nrStoch = m->VaryingStochastCount;
                 auto cm = CMsettings();
-                cm.maximumSamples = mcSettings->MaximumSamples;
-                cm.minimumSamples = mcSettings->MinimumSamples;
-                auto rnd = defaultRandomDouble();
+                cm.maximumSamples  = mcSettings->MaximumSamples;
+                cm.minimumSamples  = mcSettings->MinimumSamples;
+                cm.varCoeffFailure = mcSettings->VariationCoefficientFailure;
+                auto rnd = rndDotNet(m);
                 auto p = progress();
                 auto mc = new crudeMonteCarlo(cm, rnd, p);
                 auto parser = parseDistribution();
--- wrappers/Deltares.Reliability.Wrapper/Deltares.Reliability.Wrapper.h
+++ wrappers/Deltares.Reliability.Wrapper/Deltares.Reliability.Wrapper.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../../src/probMethods/zmodel.h"
+#include "../../src/rnd/defaultRandomDouble.h"
 #include <msclr/auto_gcroot.h>
 
 using namespace System;
@@ -10,6 +11,15 @@ using namespace Deltares::ProbLibCore;
 namespace Deltares {
     namespace Reliability {
         namespace Wrapper {
+            public class rndDotNet : public defaultRandomDouble
+            {
+            public:
+                rndDotNet(ModelRunner^ m) : mr(m) {};
+                virtual void getRandomVector(vector1D& r);
+            private:
+                msclr::auto_gcroot <ModelRunner^> mr;
+            };
+
             public class zmodelDotNet : public zmodel
             {
             public:
