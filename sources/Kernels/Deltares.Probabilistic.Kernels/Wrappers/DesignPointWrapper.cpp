2fd462d2be0b7283291c38ca6e579f34b72d46e2
robdeltares <rob.brinkman@deltares.nl>
2024-01-09 12:56:46 +0100

    #785 improved implementation of wrappers

--- sources/Kernels/Deltares.Probabilistic.Kernels/Wrappers/DesignPointWrapper.cpp
+++ sources/Kernels/Deltares.Probabilistic.Kernels/Wrappers/DesignPointWrapper.cpp
@@ -1,1 +1,17 @@
 #include "DesignPointWrapper.h"
+
+void Deltares::Probabilistic::Kernels::DesignPointWrapper::SetDesignPoint(DesignPoint* designPoint, System::Collections::Generic::List<Deltares::Probabilistic::Kernels::StochastWrapper^>^ stochasts)
+{
+	this->m_designPoint = designPoint;
+
+	SetStochastPoint(designPoint, stochasts);
+
+	this->ConvergenceReport = gcnew Deltares::Probabilistic::Kernels::ConvergenceReportWrapper(designPoint->ConvergenceReport);
+
+	for (int i = 0; i < designPoint->ReliabililityResults.size(); i++)
+	{
+		Deltares::Probabilistic::Kernels::ReliabilityResultWrapper^ resultWrapper = gcnew Deltares::Probabilistic::Kernels::ReliabilityResultWrapper(designPoint->ReliabililityResults[i]);
+		this->ReliabilityResults->Add(resultWrapper);
+	}
+}
+
