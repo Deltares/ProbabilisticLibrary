4899f55e7877b065fc20d3cb75fe197db9d389ba
Edwin Spee <edwin.spee@deltares.nl>
2024-01-24 10:50:25 +0100

    add new dotnet tests to cmake / ctest

---
+++ tests2/Program.cs
@@ -0,0 +1,35 @@
+﻿using Deltares.Probabilistics.Wrappers.Test;
+using System;
+
+// See https://aka.ms/new-console-template for more information
+try
+{
+    //Stopwatch stopWatch = System.Diagnostics.Stopwatch.StartNew();
+    var c = new TestCrudeMonteCarlo();
+    c.TestBligh();
+    c.TestConvex();
+    c.TestLinear();
+    c.TestLinearLimited();
+    c.TestLinearNegativeFullyCorrelated();
+    c.TestLinearManySamples();
+    c.TestLinearParallel();
+    c.TestLinearMessages();
+    c.TestLinearPartialCorrelated();
+    c.TestLinearProgress();
+    c.TestLinearPartialCorrelated();
+    c.TestLoadStrength();
+    c.TestLoadStrengthSurvived();
+    c.TestNoisy();
+    //c.TestNoisyInheritedModelRunner();
+    c.TestNoisyMessages();
+    c.TestNoisyStochastSettings();
+    c.TestNonLinear();
+    //stopWatch.Stop();
+    //Console.WriteLine(stopWatch.Elapsed.ToString());
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
+return 0;
+
