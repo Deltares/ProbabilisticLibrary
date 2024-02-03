70ca2f0882ecaa4518cf3d6e6a6e16770ec7cd4d
Edwin Spee <edwin.spee@deltares.nl>
2024-02-03 10:49:00 +0100

    move calculation in interface_distributions to call the c++ implementation

--- sources/Kernels/Deltares.Probabilistic.CWrapper/calcdistributionsinv.cpp
+++ sources/Kernels/Deltares.Probabilistic.CWrapper/calcdistributionsinv.cpp
@@ -20,3 +20,20 @@ void calculatedistributioninverse_c(double *u, double *y, EnumDistributions *typ
     }
 }
 
+extern "C"
+void calculatedistribution_c(double* x, double* u, EnumDistributions * type, double p[], tError * ierr)
+{
+    try
+    {
+        auto s = createDistribution::create(*type, p);
+        *u = s->getUFromX(*x);
+    }
+    catch (const std::exception& e)
+    {
+        ierr->errorCode = -1;
+        std::string s = e.what();
+        fillErrorMessage(*ierr, s);
+    }
+}
+
+
