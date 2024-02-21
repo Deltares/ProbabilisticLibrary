ecad90c88e3da77f4fb3c0b8daca5d39d57b6387
robdeltares <rob.brinkman@deltares.nl>
2024-02-21 18:32:01 +0100

    #831 using wrapper sample

--- sources/Deltares.Probabilistic.Wrapper/Model/Sample.h
+++ sources/Deltares.Probabilistic.Wrapper/Model/Sample.h
@@ -8,6 +8,32 @@ namespace Deltares
 	{
 		namespace Wrappers
 		{
+			public ref class SampleX
+			{
+			public:
+				SampleX(array<double>^ x, array<double>^ xInput, array<double>^ xFull)
+				{
+					this->X = x;
+					this->XInput = xInput;
+					this->XFull = xFull;
+				}
+
+				array<double>^ X;
+				array<double>^ XInput;
+				array<double>^ XFull;
+			};
+
+
+			/// <summary>
+			/// Indicates the meaning of <see cref="Sample.Values"/> in a <see cref="Sample"/>
+			/// </summary>
+			public enum class SpaceType { U, X };
+
+			/// <summary>
+			/// Indicates whether a <see cref="Sample"/> should be logged
+			/// </summary>
+			public enum class LogSampleType { Never, OnlyModel, Always };
+
 			using namespace Deltares::Models;
 			using namespace Deltares::Utils::Wrappers;
 
@@ -71,23 +97,6 @@ namespace Deltares
 					void set(double value) { shared->object->Z = value; }
 				}
 
-				property double Beta
-				{
-					double get()
-					{
-						if (values != nullptr)
-						{
-							// synchronize values
-							for (int i = 0; i < values->Length; i++)
-							{
-								shared->object->Values[i] = values[i];
-							}
-						}
-
-						return shared->object->getBeta();
-					}
-				}
-
 				property System::Object^ Tag
 				{
 					System::Object^ get()
@@ -104,6 +113,170 @@ namespace Deltares
 						tag = value;
 						shared->object->Tag = NativeSupport::toNativeObject(value);
 					}
+				}
+
+				// TODO: next methods and properties should be removed after after c++ conversion
+
+				Sample(SpaceType spaceType, int count) : Sample(spaceType, -1, count)
+				{
+				}
+
+				Sample(SpaceType spaceType, int iteration, int count) : Sample(spaceType, iteration, gcnew array<double>(count))
+				{
+				}
+
+				Sample(SpaceType spaceType, array<double>^ values) : Sample(spaceType, -1, values)
+				{
+				}
+
+				Sample(SpaceType spaceType, int iteration, array<double>^ values) : Sample(values)
+				{
+					this->SpaceType = spaceType;
+					this->Iteration = iteration;
+				}
+
+				Sample(SpaceType spaceType, int iteration, array<double>^ values, LogSampleType logSampleType) : Sample(values)
+				{
+					this->SpaceType = spaceType;
+					this->Iteration = iteration;
+					this->LogSampleType = logSampleType;
+				}
+
+				/// <summary>
+				/// The space type in which the <see cref="Values"/> are defined
+				/// </summary>
+				SpaceType SpaceType = SpaceType::U;
+
+				LogSampleType LogSampleType = LogSampleType::Always;
+
+				double Factor = 0;
+
+				SampleX^ X = nullptr;
+
+				bool IsNew()
+				{
+					return this->X == nullptr;
+				}
+
+				Sample^ GetSampleAtBeta(double beta)
+				{
+					UpdateValues();
+
+					array<double>^ values = NativeSupport::toManaged(shared->object->getSampleAtBeta(beta)->Values);
+
+					return gcnew Sample(this->SpaceType, this->Iteration, values);
+				}
+
+				Sample^ GetNormalizedSample()
+				{
+					UpdateValues();
+
+					array<double>^ values = NativeSupport::toManaged(shared->object->getNormalizedSample()->Values);
+
+					return gcnew Sample(this->SpaceType, this->Iteration, values);
+				}
+
+				Sample^ GetMultipliedSample(double factor)
+				{
+					UpdateValues();
+
+					array<double>^ values = NativeSupport::toManaged(shared->object->getMultipliedSample(factor)->Values);
+
+					return gcnew Sample(this->SpaceType, this->Iteration, values);
+				}
+
+				Sample^ Clone()
+				{
+					UpdateValues();
+
+					array<double>^ values = NativeSupport::toManaged(shared->object->clone()->Values);
+
+					//array<double>^ clonedValues = gcnew array<double>(this->Values->Length);
+					//for (int i = 0; i < clonedValues->Length; i++)
+					//{
+					//	clonedValues[i] = this->Values[i];
+					//}
+
+					return gcnew Sample(this->SpaceType, this->Iteration, values);
+				}
+
+				/// <summary>
+				/// Tells whether this sample should be saved
+				/// </summary>
+				/// <param name="reused"></param>
+				/// <returns></returns>
+				bool ShouldSave(bool reused)
+				{
+					return this->LogSampleType == LogSampleType::Always || (this->LogSampleType == LogSampleType::OnlyModel && !reused);
+				}
+
+				int ScenarioIndex = -1;
+
+				void UpdateValues()
+				{
+					if (values != nullptr)
+					{
+						// synchronize values
+						for (int i = 0; i < values->Length; i++)
+						{
+							shared->object->Values[i] = values[i];
+						}
+					}
+				}
+
+				// TODO: remove after c++ conversion
+				property double Beta
+				{
+					double get()
+					{
+						UpdateValues();
+						return shared->object->getBeta();
+					}
+				}
+
+				/// <summary>
+/// Gets the squared distance to another sample
+/// </summary>
+/// <param name="other"></param>
+/// <returns></returns>
+				double GetDistance2(Sample^ other, bool hasWeighting)
+				{
+					double sum = 0;
+					for (int j = 0; j < other->Values->Length; ++j)
+					{
+						double weight = 1;
+						if (hasWeighting) weight = other->Weight;
+						double diff = (this->Values[j] - other->Values[j]) * weight;
+						sum += diff * diff;
+					}
+
+					return sum;
+				}
+
+				bool AreValuesEqual(Sample^ other)
+				{
+					if (this == other)
+					{
+						return true;
+					}
+
+					if (this->Values->Length != this->Values->Length)
+					{
+						return false;
+					}
+
+					for (int i = 0; i < this->Values->Length; i++)
+					{
+						if (!isnan(this->Values[i]) || !isnan(other->Values[i]))
+						{
+							if (this->Values[i] != other->Values[i])
+							{
+								return false;
+							}
+						}
+					}
+
+					return true;
 				}
 			};
 		}
