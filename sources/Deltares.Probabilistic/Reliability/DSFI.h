edf60b795a3fda500df809f54e617ddcea7bdcae
Edwin Spee <edwin.spee@deltares.nl>
2024-03-08 16:23:31 +0100

    tabs => spaces

--- sources/Deltares.Probabilistic/Reliability/DSFI.h
+++ sources/Deltares.Probabilistic/Reliability/DSFI.h
@@ -6,14 +6,14 @@
 
 namespace Deltares
 {
-	namespace Reliability
-	{
-		class DSFI : public ReliabilityMethod
-		{
-		public:
-			std::shared_ptr<FORMSettings> formSettings = std::make_shared<FORMSettings>();
-			std::shared_ptr<DirectionalSamplingSettings> DsSettings = std::make_shared<DirectionalSamplingSettings>();
-			std::shared_ptr<DesignPoint> getDesignPoint(std::shared_ptr<Models::ModelRunner> modelRunner) override;
-		};
-	}
+    namespace Reliability
+    {
+        class DSFI : public ReliabilityMethod
+        {
+        public:
+            std::shared_ptr<FORMSettings> formSettings = std::make_shared<FORMSettings>();
+            std::shared_ptr<DirectionalSamplingSettings> DsSettings = std::make_shared<DirectionalSamplingSettings>();
+            std::shared_ptr<DesignPoint> getDesignPoint(std::shared_ptr<Models::ModelRunner> modelRunner) override;
+        };
+    }
 }
--- sources/Deltares.Probabilistic/Reliability/FDIR.cpp
+++ sources/Deltares.Probabilistic/Reliability/FDIR.cpp
@@ -4,23 +4,23 @@
 
 namespace Deltares
 {
-	namespace Reliability
-	{
-		std::shared_ptr<DesignPoint> FDIR::getDesignPoint(std::shared_ptr<Models::ModelRunner> modelRunner)
-		{
-			auto form = FORM();
-			form.Settings = formSettings;
-			auto formDesignPoint = form.getDesignPoint(modelRunner);
-			if (formDesignPoint.get()->convergenceReport->IsConverged)
-			{
-				return formDesignPoint;
-			}
+    namespace Reliability
+    {
+        std::shared_ptr<DesignPoint> FDIR::getDesignPoint(std::shared_ptr<Models::ModelRunner> modelRunner)
+        {
+            auto form = FORM();
+            form.Settings = formSettings;
+            auto formDesignPoint = form.getDesignPoint(modelRunner);
+            if (formDesignPoint.get()->convergenceReport->IsConverged)
+            {
+                return formDesignPoint;
+            }
 
-			auto ds = DirectionalSampling();
-			ds.Settings = DsSettings;
-			auto dsDesignPoint = ds.getDesignPoint(modelRunner);
-			dsDesignPoint->convergenceReport->TotalIterations = formDesignPoint->convergenceReport->TotalIterations;
-			return dsDesignPoint;
-		}
-	}
+            auto ds = DirectionalSampling();
+            ds.Settings = DsSettings;
+            auto dsDesignPoint = ds.getDesignPoint(modelRunner);
+            dsDesignPoint->convergenceReport->TotalIterations = formDesignPoint->convergenceReport->TotalIterations;
+            return dsDesignPoint;
+        }
+    }
 }
