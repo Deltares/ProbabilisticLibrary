b9f931e5520a2492aff20e90192759f6fc18aa5c
Edwin Spee <edwin.spee@deltares.nl>
2024-03-28 14:09:51 +0100

    add getPQfromU to standardNormal

--- sources/Deltares.Probabilistic/Statistics/StandardNormal.h
+++ sources/Deltares.Probabilistic/Statistics/StandardNormal.h
@@ -17,6 +17,7 @@ namespace Deltares
 			static double getUFromQ(const double q);
 			static double getRFromP(const double p);
 			static double getPFromR(const double r);
+			static void getPQfromU(const double u, double& p, double& q);
 			static double ReturnTimeFromBeta(const double beta);
 			static double FreqFromBeta(const double beta);
 			static double LogQFromBeta(const double beta);
