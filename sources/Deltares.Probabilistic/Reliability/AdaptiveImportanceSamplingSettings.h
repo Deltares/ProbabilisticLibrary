d2e86177042f039eaf33def94f96222ee814eb36
robdeltares <rob.brinkman@deltares.nl>
2024-04-17 12:04:12 +0200

    #21 added adaptive importance samling settings

---
+++ sources/Deltares.Probabilistic/Reliability/AdaptiveImportanceSamplingSettings.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "../Model/RunSettings.h"
+#include "StartPointCalculatorSettings.h"
+#include "StochastSettingsSet.h"
+
+namespace Deltares
+{
+	namespace Reliability
+	{
+		class AdaptiveImportanceSamplingSettings
+		{
+		public:
+			int MaxVarianceLoops = 5;
+			bool AutoMaximumSamplesNoResult = false;
+			int MaximumSamplesNoResult = 10000;
+			int MaximumSamples = 10000;
+			int MinimumFailedSamples = 0;
+			double LoopVarianceIncrement = 0.5;
+			bool StartPointOnLimitState = false;
+			double RestartStepSize = 0;
+			double VarianceFactor= 1.5;
+			int MinVarianceLoops = 1;
+			double EpsWeightSample = 0.1;
+			double FractionFailed = 0.1;
+			double MaxBeta = Statistics::StandardNormal::BetaMax;
+
+			std::shared_ptr<Models::RunSettings> RunSettings = std::make_shared<Models::RunSettings>();
+			std::shared_ptr<StartPointCalculatorSettings> StartPointSettings = std::make_shared<StartPointCalculatorSettings>();
+			std::shared_ptr<StochastSettingsSet> StochastSet = std::make_shared<StochastSettingsSet>();
+
+			bool isValid()
+			{
+				return MinVarianceLoops >= 1 &&
+					MaxVarianceLoops >= MinVarianceLoops &&
+					RestartStepSize >= 0 &&
+					FractionFailed >= 0 && FractionFailed <= 0.5 &&
+					StartPointSettings->isValid() &&
+					RunSettings->isValid();
+			}
+		};
+	}
+}
+
