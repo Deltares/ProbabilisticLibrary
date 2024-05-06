8edf9af5759f40f636e07e91c7f67d7f7c5683b3
robdeltares <rob.brinkman@deltares.nl>
2024-05-06 13:57:45 +0200

    #11 numerical integration

---
+++ sources/Deltares.Probabilistic.Wrapper/Reliability/NumericalIntegration.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "NumericalIntegrationSettings.h"
+#include "ReliabilityMethod.h"
+#include "../../Deltares.Probabilistic/Reliability/NumericalIntegration.h"
+
+namespace Deltares
+{
+	namespace Reliability
+	{
+		namespace Wrappers
+		{
+			using namespace Deltares::Utils::Wrappers;
+			using namespace Deltares::Models::Wrappers;
+
+			public ref class NumericalIntegration : public ReliabilityMethod
+			{
+			private:
+				SharedPointerProvider<Reliability::NumericalIntegration>* shared = new SharedPointerProvider(new Reliability::NumericalIntegration());
+			public:
+				NumericalIntegration() {}
+				~NumericalIntegration() { this->!NumericalIntegration(); }
+				!NumericalIntegration() { delete shared; }
+
+				std::shared_ptr<Reliability::ReliabilityMethod> GetReliabilityMethod() override
+				{
+					shared->object->Settings = Settings->GetSettings();
+					return shared->object;
+				}
+
+				NumericalIntegrationSettings^ Settings = gcnew NumericalIntegrationSettings();
+
+				System::Object^ GetSettings() override { return Settings; }
+
+				bool IsValid() override { return Settings->IsValid(); }
+
+				void Stop()	override { shared->object->Stop(); }
+
+				bool IsStopped() override { return shared->object->isStopped(); }
+			};
+		}
+	}
+}
+
+
