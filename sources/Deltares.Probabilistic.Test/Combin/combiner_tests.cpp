664364e7c95063b3897a85d5e15ecc0c64cd6774
Edwin Spee <edwin.spee@deltares.nl>
2024-06-17 14:32:14 +0200

    make combiner test independ of upscaling and use less elements

--- sources/Deltares.Probabilistic.Test/Combin/combiner_tests.cpp
+++ sources/Deltares.Probabilistic.Test/Combin/combiner_tests.cpp
@@ -1,11 +1,10 @@
 #include <fstream>
-#include <stdio.h>
 #include "gtest/gtest.h"
 #include "combiner_tests.h"
+#include "../../Deltares.Probabilistic/Math/NumericSupport.h"
 #include "../../Deltares.Probabilistic/Combine/HohenbichlerCombiner.h"
 #include "../../Deltares.Probabilistic/Combine/DirectionalSamplingCombiner.h"
 #include "../../Deltares.Probabilistic/Combine/ImportanceSamplingCombiner.h"
-#include "../../Deltares.Probabilistic/Combine/upscaling.h"
 
 using namespace Deltares::Reliability;
 using namespace Deltares::Statistics;
@@ -25,11 +24,10 @@ namespace Deltares
 
             void CombinerTest::HohenbichlerCombinerTest()
             {
-                margin = 1e-7;
+                margin = 1e-6;
                 auto hh = std::make_unique<HohenbichlerCombiner>();
-                auto ref = alphaBeta(1.9791111899137395, // pre-computed
-                    { 0.70367242324781842, 0.43749837909101225, 0.47566443271219239, 0.29526875301302696 }); // pre-computed
-                tester(hh.get(), ref);
+                auto ref = alphaBeta(2.78586434, { 0.6145053, 0.378711, 0.5892747, 0.3629275 }); // pre-computed
+                tester(hh.get(), 3.0, ref);
             }
 
             void CombinerTest::DirectionalSamplingCombinerTest()
@@ -37,9 +35,8 @@ namespace Deltares
                 margin = 0.1;
                 auto dsCombiner = std::make_unique<DirectionalSamplingCombiner>();
                 dsCombiner->randomGeneratorType = Numeric::MersenneTwister;
-                auto ref = alphaBeta(3.0139519, // pre-computed
-                    { 0.56572, 0.41313, 0.58968, 0.40193 }); // pre-computed
-                tester(dsCombiner.get(), ref);
+                auto ref = alphaBeta(3.0139519, { 0.56572, 0.41313, 0.58968, 0.40193 }); // pre-computed
+                tester(dsCombiner.get(), 3.0, ref);
             }
 
             void CombinerTest::ImportanceSamplingCombinerTest()
@@ -48,14 +45,19 @@ namespace Deltares
                 auto importance_sampling_combiner = std::make_unique<ImportanceSamplingCombiner>();
                 importance_sampling_combiner->randomGeneratorType = Numeric::MersenneTwister;
                 auto ref = alphaBeta(3.0, { 0.6, 0.374, 0.6, 0.374}); // pre-computed
-                tester(importance_sampling_combiner.get(), ref);
+                tester(importance_sampling_combiner.get(), 3.0, ref);
             }
 
             // test based on testcombineMultipleElementsSpatialCorrelated1
-            void CombinerTest::tester(Combiner* comb, const alphaBeta& ref)
+            void CombinerTest::tester(Combiner* comb, const double beta, const alphaBeta& ref)
             {
-                const int nElements = 20; // Number of elements
+                const int nElements = 2; // Number of elements
                 const size_t nStochasts = 4;
+
+                auto rhoXK = std::vector<double>({ 0.5, 0.5, 0.2, 0.2 });
+                auto alphaInput = std::vector<double>{ 0.6, 0.37, 0.6, 0.37 };
+                auto length = Numeric::NumericSupport::GetLength(alphaInput);
+                for (auto x : alphaInput) { x /= length; }
 
                 std::vector< std::shared_ptr<Stochast>> stochasts;
                 for (size_t i = 0; i < nStochasts; i++)
@@ -67,23 +69,15 @@ namespace Deltares
                     stochasts.push_back(s);
                 }
 
-                auto CrossSection = alphaBeta(3.0, { 0.6, sqrt(0.5 - 0.36), 0.6, sqrt(0.5 - 0.36) });
-                auto rhoXK = vector1D({ 0.5, 0.5, 0.2, 0.2 });
-                auto dXK = vector1D({ 500.0, 300.0, 500.0, 300.0 });
-                double sectionLength = 100.0;
-
-                auto up = upscaling();
-                auto section = up.upscaleLength(CrossSection, rhoXK, dXK, sectionLength, -999.0);
-                EXPECT_EQ(section.second, 0);
                 auto Elements = std::vector<std::shared_ptr<DesignPoint>>();
                 for (size_t i = 0; i < nElements; i++)
                 {
                     auto dp = std::make_shared<DesignPoint>();
-                    dp->Beta = section.first.getBeta();
+                    dp->Beta = beta;
                     for (size_t j = 0; j < nStochasts; j++)
                     {
                         auto alpha = std::make_shared<StochastPointAlpha>();
-                        alpha->Alpha = section.first.getAlphaI(j);
+                        alpha->Alpha = alphaInput[j];
                         alpha->Stochast = stochasts[j];
                         alpha->U = -dp->Beta * alpha->Alpha;
                         dp->Alphas.push_back(alpha);
@@ -94,7 +88,7 @@ namespace Deltares
                 auto rho = std::make_shared<SelfCorrelationMatrix>();
                 for (size_t i = 0; i < nStochasts; i++)
                 {
-                    rho->setSelfCorrelation(Elements[0]->Alphas[i]->Stochast, rhoXK(i));
+                    rho->setSelfCorrelation(Elements[0]->Alphas[i]->Stochast, rhoXK[i]);
                 }
 
                 auto cmbDp = comb->combineDesignPoints(combineAndOr::combOr, Elements, rho, nullptr);
--- sources/Deltares.Probabilistic.Test/Combin/combiner_tests.h
+++ sources/Deltares.Probabilistic.Test/Combin/combiner_tests.h
@@ -16,7 +16,7 @@ namespace Deltares
                 void DirectionalSamplingCombinerTest();
                 void ImportanceSamplingCombinerTest();
             private:
-                void tester(Reliability::Combiner* comb, const Reliability::alphaBeta& ref);
+                void tester(Reliability::Combiner* comb, const double beta, const Reliability::alphaBeta& ref);
                 double margin = 1.0e-7;
             };
         }
