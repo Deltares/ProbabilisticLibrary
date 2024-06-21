dd2a8bdfd39a68728c658289bca872aa19ef0169
robdeltares <rob.brinkman@deltares.nl>
2024-06-21 10:31:49 +0200

    PROBL-46 added tests and reorganized python files

--- sources/Deltares.Probabilistic.PWrapper.Test/project_builder.py
+++ sources/Deltares.Probabilistic.PWrapper.Test/project_builder.py
@@ -1,14 +1,14 @@
 import sys
 import interface
-import model
 
-from model import Stochast
-from model import Settings
-from model import Project
-
+from stochast import *
+from project import *
 
 def linear(values):
     return 1.8 - sum(values)
+
+def linear_small(values):
+    return 1.98 - sum(values)
 
 
 def get_linear_project():
@@ -28,6 +28,26 @@ def get_linear_project():
     project.variables.append(stochast2)
 
     project.model = linear
+
+    return project
+
+def get_linear_small_project():
+
+    project = Project()
+
+    stochast1 = Stochast()
+    stochast1.distribution = "uniform"
+    stochast1.minimum = -1
+    stochast1.maximum = 1;
+    project.variables.append(stochast1)
+
+    stochast2 = Stochast()
+    stochast2.distribution = "uniform"
+    stochast2.minimum = -1
+    stochast2.maximum = 1;
+    project.variables.append(stochast2)
+
+    project.model = linear_small
 
     return project
 
