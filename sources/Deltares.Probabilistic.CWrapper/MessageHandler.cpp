eef98b4bc9712be1a52720f373338d3cf57bbfa2
Edwin Spee <edwin.spee@deltares.nl>
2024-06-24 10:22:24 +0200

    fixes for the gnu/coverage build

--- sources/Deltares.Probabilistic.CWrapper/MessageHandler.cpp
+++ sources/Deltares.Probabilistic.CWrapper/MessageHandler.cpp
@@ -1,53 +1,58 @@
 #include "../Deltares.Probabilistic/Model/ProjectServer.h"
 
 #include <string>
+#ifdef __GNUC__
+#define DLL_PUBLIC __attribute__ ((visibility("default")))
+#else
+#define DLL_PUBLIC __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
+#endif
 
 std::shared_ptr<Deltares::Models::ProjectServer> projectServer = std::make_shared< Deltares::Models::ProjectServer>();
 
-extern "C" __declspec(dllexport) int Create(char* type)
+extern "C" DLL_PUBLIC int Create(char* type)
 {
     std::string typeStr = type;
     int id = projectServer->Create(typeStr);
     return id;
 }
 
-extern "C" __declspec(dllexport) double GetValue(int id, char* property)
+extern "C" DLL_PUBLIC double GetValue(int id, char* property)
 {
     std::string propertyStr(property);
     return projectServer->GetValue(id, propertyStr);
 }
 
-extern "C" __declspec(dllexport) void SetValue(int id, char* property, double value)
+extern "C" DLL_PUBLIC void SetValue(int id, char* property, double value)
 {
     std::string propertyStr(property);
     projectServer->SetValue(id, propertyStr, value);
 }
 
-extern "C" __declspec(dllexport) int GetIntValue(int id, char* property)
+extern "C" DLL_PUBLIC int GetIntValue(int id, char* property)
 {
     std::string propertyStr(property);
     return projectServer->GetIntValue(id, propertyStr);
 }
 
-extern "C" __declspec(dllexport) void SetIntValue(int id, char* property, int value)
+extern "C" DLL_PUBLIC void SetIntValue(int id, char* property, int value)
 {
     std::string propertyStr(property);
     projectServer->SetIntValue(id, propertyStr, value);
 }
 
-extern "C" __declspec(dllexport) bool GetBoolValue(int id, char* property)
+extern "C" DLL_PUBLIC bool GetBoolValue(int id, char* property)
 {
     std::string propertyStr(property);
     return projectServer->GetBoolValue(id, propertyStr);
 }
 
-extern "C" __declspec(dllexport) void SetBoolValue(int id, char* property, bool value)
+extern "C" DLL_PUBLIC void SetBoolValue(int id, char* property, bool value)
 {
     std::string propertyStr(property);
     projectServer->SetBoolValue(id, propertyStr, value);
 }
 
-extern "C" __declspec(dllexport) const char* GetStringValue(int id, char* property)
+extern "C" DLL_PUBLIC const char* GetStringValue(int id, char* property)
 {
     std::string propertyStr(property);
     std::string result = projectServer->GetStringValue(id, propertyStr);
@@ -55,14 +60,14 @@ extern "C" __declspec(dllexport) const char* GetStringValue(int id, char* proper
     return resultC;
 }
 
-extern "C"  __declspec(dllexport) void SetStringValue(int id, char* property, char* value)
+extern "C"  DLL_PUBLIC void SetStringValue(int id, char* property, char* value)
 {
     std::string propertyStr(property);
     std::string valueStr(value);
     projectServer->SetStringValue(id, propertyStr, valueStr);
 }
 
-extern "C" __declspec(dllexport) int* GetArrayValue(int id, char* property)
+extern "C" DLL_PUBLIC int* GetArrayValue(int id, char* property)
 {
     std::string propertyStr(property);
     std::vector<int> values = projectServer->GetArrayValue(id, propertyStr);
@@ -73,37 +78,37 @@ extern "C" __declspec(dllexport) int* GetArrayValue(int id, char* property)
     return data;
 }
 
-extern "C" __declspec(dllexport) void SetArrayValue(int id, char* property, int* values, int size)
+extern "C" DLL_PUBLIC void SetArrayValue(int id, char* property, int* values, int size)
 {
     std::string propertyStr(property);
     projectServer->SetArrayValue(id, propertyStr, values, size);
 }
 
-extern "C"  __declspec(dllexport) double GetArgValue(int id, char* property, double argument)
+extern "C" DLL_PUBLIC double GetArgValue(int id, char* property, double argument)
 {
     std::string propertyStr(property);
     return projectServer->GetArgValue(id, propertyStr, argument);
 }
 
-extern "C" __declspec(dllexport) void SetArgValue(int id, char* property, double argument, double value)
+extern "C" DLL_PUBLIC void SetArgValue(int id, char* property, double argument, double value)
 {
     std::string propertyStr(property);
     projectServer->SetArgValue(id, propertyStr, argument, value);
 }
 
-extern "C"  __declspec(dllexport) int GetIndexedIntValue(int id, char* property, int index)
+extern "C" DLL_PUBLIC int GetIndexedIntValue(int id, char* property, int index)
 {
     std::string propertyStr(property);
     return projectServer->GetIndexedIntValue(id, propertyStr, index);
 }
 
-extern "C" __declspec(dllexport) void SetCallBack(int id, char* property, Deltares::Models::ZValuesCallBack callBack)
+extern "C" DLL_PUBLIC void SetCallBack(int id, char* property, Deltares::Models::ZValuesCallBack callBack)
 {
     std::string propertyStr(property);
     projectServer->SetCallBack(id, propertyStr, callBack);
 }
 
-extern "C" __declspec(dllexport) void Execute(int id, char* method)
+extern "C" DLL_PUBLIC void Execute(int id, char* method)
 {
     std::string methodStr(method);
     projectServer->Execute(id, methodStr);
