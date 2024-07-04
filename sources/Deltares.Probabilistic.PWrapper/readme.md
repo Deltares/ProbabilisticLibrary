49ed6e2a6115c82dc478f778d6ea6989d47cf60d
robdeltares <rob.brinkman@deltares.nl>
2024-07-04 15:24:48 +0200

    PROBL-51 making wheel

---
+++ sources/Deltares.Probabilistic.PWrapper/readme.md
@@ -0,0 +1,6 @@
+# Deltares Probabilistic Library
+
+This is the awesome Deltares Probabilistic Library
+
+## A second-level heading
+### A third-level heading
---
+++ sources/Deltares.Probabilistic.PWrapper/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+ 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+ 
+setuptools.setup(
+    name="d-rely",
+    version="0.0.1",
+    author="Deltares",
+    author_email="software.support@deltares.nl",
+    description="Package to perform probabilistic analyses",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    package_data={'d-rely':['bin/Deltares.Probabilistic.CWrapper.dll']},    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: AGPL License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
+
