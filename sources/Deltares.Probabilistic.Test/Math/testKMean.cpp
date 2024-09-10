f0f9ed712c3bb105003d80c7ace2713d3d726aef
Edwin Spee <edwin.spee@deltares.nl>
2024-09-10 11:03:06 +0200

    bulk change: rename Probabilistic Library into Streams in sources

--- sources/Deltares.Probabilistic.Test/Math/testKMean.cpp
+++ sources/Deltares.Probabilistic.Test/Math/testKMean.cpp
@@ -2,7 +2,7 @@
 //
 // This file is part of the Probabilistic Library.
 //
-// The Probabilistic Library is free software: you can redistribute it and/or modify
+// Streams is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
