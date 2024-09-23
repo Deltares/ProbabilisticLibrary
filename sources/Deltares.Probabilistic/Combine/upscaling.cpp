0eea2d27f8c1a67ee1b8529eab5f9ae82d5fca53
Edwin Spee <edwin.spee@deltares.nl>
2024-09-23 13:07:19 +0200

    improve internal documentation

--- sources/Deltares.Probabilistic/Combine/upscaling.cpp
+++ sources/Deltares.Probabilistic/Combine/upscaling.cpp
@@ -34,47 +34,32 @@ using namespace Deltares::Statistics;
 namespace Deltares {
     namespace Reliability {
 
-        //
-        // Method for combining failure probabilities over equal elements, with exceptions for correlations close to zero
-        //
+        // \brief Method for combining failure probabilities over equal elements, with exceptions for correlations close to zero
+        // \param nrTimes   : Number of time the original time
+        // \param beta      : Reliability index of a single time element
+        // \param alpha(:)  : Influence coefficients of a single time element
+        // \param inRhoT(:) : Correlation coefficients for each of the variables, in time
         int upscaling::upscaleInTime(const double nrTimes, alphaBeta& element, const vector1D& inRhoT)
         {
-            // nrTimes   : Number of time the original time
-            // beta      : Reliability index of a single time element
-            // alpha(:)  : Influence coefficients of a single time element
-            // inRhoT(:) : Correlation coefficients for each of the variables, in time
-
-            // nStochasts : Number of stochastic variables
-            // rhoT       : Correlation coefficient between element 1 and element 2
-            // Pf         : Failure probability
-            // PfT        : Failure probability of the combined element
-            // betaT      : Beta value for the combined element
-            // alphaT(:)  : Equivalent alpha values for all variables, of the combined element
-            // betaK      : Perturbed initial beta, part of computation of equivalent alpha values
-            // PfTK       : Failure probability from perturbation of u, part of computation of equivalent alpha values
-            // betaTK     : Beta value from perturbation of u, part of computation of equivalent alpha values
-            // alphaO     : Uncorrelated part of the equivalent alpha value
-            // alphaC     : Correlated part of the equivalent alpha value
-
             const double rhoMax = 1.0 - 1.0e-10;  // Max allowed value of correlation coefficient (very close to one)
             const double rhoMin = 1.0e-10;        // Min allowed value of correlation coefficient (very close to zero)
             const double epsi = 0.01;           // Perturbation amount, part of computation of equivalent alpha values
 
-            size_t nStochasts = element.size();
+            size_t nStochasts = element.size(); // Number of stochastic variables
             int failures = 0;
 
-            auto alphaT = vector1D(nStochasts);
+            auto alphaT = vector1D(nStochasts); // Equivalent alpha values for all variables, of the combined element
 
             //
             // Determine the correlation between two time elements
             //
-            double rhoT = element.sumOfInners(element, inRhoT);
+            double rhoT = element.sumOfInners(element, inRhoT); // rhoT : Correlation coefficient between element 1 and element 2
             rhoT = std::max(std::min(rhoT, rhoMax), rhoMin);
 
             //
             // Compute failure probability and beta of the combined n elements
             //
-            auto betaT = upscaleBeta(element.getBeta(), rhoT, nrTimes, failures);
+            auto betaT = upscaleBeta(element.getBeta(), rhoT, nrTimes, failures); // Beta value for the combined element
 
             //
             // Get equivalent alpha and beta values for the combined element in 6 steps
@@ -90,17 +75,23 @@ namespace Deltares {
             //
             // Step 2/6: Compute the beta of the combined element, based on perturbed initial beta
             //
+
+            // Beta value from perturbation of u, part of computation of equivalent alpha values
             auto betaTK = upscaleBeta(bk, rhoT, nrTimes, failures);
 
             //
             // Step 3/6: Compute the correlated part of alpha (dependant on the sign of the pertubation)
             //
+
+            // alphaC : Correlated part of the equivalent alpha value
             double alphaC = std::min(1.0, std::max(-1.0, (betaT - betaTK) / epsi));
             if (alphaC != 1.0) // TODO: compare real with tol 1e-12 or so
             {
                 //
                 // Step 4/6: Compute the alpha value for the uncorrelated part of alpha
                 //
+
+                // alphaO : Uncorrelated part of the equivalent alpha value
                 double alphaO = sqrt(1.0 - alphaC * alphaC);
 
                 //
@@ -152,17 +143,16 @@ namespace Deltares {
             }
         }
 
-        //
-        // This method upscales from a cross section to a given section length
+        // \brief This method upscales from a cross section to a given section length
+        // \param crossSectionElement : Reliability index and alpha cross section
+        // \param rhoXK(:) : Correlation variables
+        // \param dXK(:) : Correlation length variables
+        // \param sectionLength : Section length
+        // \param BreachLength : Breach length (mechanism width)
+        // \return Reliability index, alpha for section and indication of non-converged Hohenbichler calculations
         std::pair<alphaBeta, int> upscaling::upscaleLength(alphaBeta& crossSectionElement,
             const vector1D& rhoXK, const vector1D& dXK, const double sectionLength, double breachL)
         {
-            // crossSectionElement  : Reliability index and alpha cross section
-            // rhoXK(:)             : Correlation variables
-            // dXK(:)               : Correlation length variables
-            // sectionLength        : Section length
-            // BreachLength         : Breach length (mechanism width)
-
             const double deltaBeta = 0.01; // perturbation of beta in computation alpha in the upscaling from cross section to segment
             //
             // Get number of variables
@@ -267,53 +257,41 @@ namespace Deltares {
             }
         }
 
-        //
-        // Method used in upscaling for computing the beta of a section from the beta of a cross section.
-        //
+        // \brief Method used in upscaling for computing the beta of a section from the beta of a cross section.
+        // \param betaCrossSection : Reliability index of the cross section
+        // \param sectionLength : Length of the section
+        // \param breachL : Breach length
+        // \param rhoZ : Correlation Z-function
+        // \param dz : Correlation length
+        // \param deltaL : Delta L
+        // \return Reliability index for the section and indication of non-converged Hohenbichler calculation
         std::pair<double, int> upscaling::ComputeBetaSection(const double betaCrossSection, const double sectionLength,
             const double breachL, const double rhoZ, const double dz, const double deltaL)
         {
-            // betaCrossSection : Reliability index of the cross section
-            // sectionLength    : Length of the section
-            // breachL          : Breach length
-            // rhoZ             : Correlation Z-function
-            // dz               : Correlation length
-            // deltaL           : Delta L
-            // betaSection      : Reliability index of the section after upscaling
-
             const int    nGridPoints = 30001;    // Number gridpoints for v in numerical integration
             const double vLower = -30.0;  // Lower bound of v-values in the numerical integration
             const double vUpper = 30.0;  // Upper bound of v-values in the numerical integration
-            // vDelta             : Step size in the numerical integration over v in [vLower, vUpper]
-            // x                  : x value
-            // v                  : v value
-            // p                  : Probability
-            // pf                 : Failure probability
-            // nf                 : Number of cross section
-            // pfX                : Failure probability section
-            // pfVV               : Failure probability vv, Hohenbichler
-            // termI(nGridPoints) : i-th term to add
 
         //   Compute failure probability cross section from beta
-            double pf = StandardNormal::getQFromU(betaCrossSection);
-            double pfX;
+            double pf = StandardNormal::getQFromU(betaCrossSection); // Failure probability cross section
+            double pfX; // Failure probability section
             int conv = 0;
             if (rhoZ > 0.001)
             {
-                double vDelta = (vUpper - vLower) / double(nGridPoints - 1);
-                double p = StandardNormal::getPFromU(betaCrossSection);
+                double vDelta = (vUpper - vLower) / double(nGridPoints - 1); // Step size in the numerical integration over v in [vLower, vUpper]
+                double p = StandardNormal::getPFromU(betaCrossSection); // Probability
                 if (sectionLength <= breachL)
                 {
                     pfX = pf;
                 }
                 else
                 {
-                    auto termI = std::vector<double>(nGridPoints + 1);
+                    auto termI = std::vector<double>(nGridPoints + 1); // i-th term to add
                     for (size_t i = 1; i <= nGridPoints; i++)
                     {
                         double v = vLower + vDelta * double(i - 1);
                         double x = (betaCrossSection - sqrt(rhoZ) * v) / sqrt(1.0 - rhoZ); // x = beta*
-                        double nf = std::max((sectionLength - breachL), 0.0) / (sqrt(2.0) * M_PI) / dz * exp(-x * x / 2.0);
+                        double nf = std::max((sectionLength - breachL), 0.0) / (sqrt(2.0) * M_PI) / dz * exp(-x * x / 2.0); // Number of cross section
                         termI[i] = (1.0 - p * exp(-nf)) * exp(-v * v / 2.0) / sqrt(2.0 * M_PI) * vDelta;
                     }
                     //
@@ -332,7 +310,7 @@ namespace Deltares {
             }
             else
             {
-                auto pfVV = hhb.PerformHohenbichler(betaCrossSection, pf, rhoZ);
+                auto pfVV = hhb.PerformHohenbichler(betaCrossSection, pf, rhoZ); // Failure probability vv, Hohenbichler
                 conv = pfVV.second;
                 pfX = pf + std::max((sectionLength - breachL), 0.0) / deltaL * (pf - pfVV.first * pf);
                 pfX = std::min(pfX, 1.0);
@@ -342,17 +320,17 @@ namespace Deltares {
             return { betaSection, conv };
         }
 
-        // Method for upscaling random variables to the largest block duration
+        // \brief Method for upscaling random variables to the largest block duration
+        // \param smallBlock : Reliability index and alpha input
+        // \param rhoTSmallBlock(:) : Correlations
+        // \param blockDurations(:) : Block durations vector input
+        // \param largestBlockDuration : Target block duration
+        // \param largestBlock : Reliability index and alpha result
+        // \param durationsLargestBlock(:) : Block durations vector result
         void upscaling::upscaleToLargestBlock(const alphaBeta& smallBlock,
             const vector1D& rhoTSmallBlock, const vector1D& blockDurations, const double largestBlockDuration,
             alphaBeta& largestBlock, vector1D& durationsLargestBlock)
         {
-            // smallBlock               : Reliability index and alpha input
-            // rhoTSmallBlock(:)        : Correlations
-            // blockDurations(:)        : Block durations vector input
-            // largestBlockDuration     : Target block duration
-            // largestBlock             : Reliability index and alpha result
-            // durationsLargestBlock(:) : Block durations vector result
 
             //
             // Get number of strength and load variables
