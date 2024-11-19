90da5dfce7a1e5c47c1bfdb5daaf83c94dad3463
Edwin Spee <edwin.spee@deltares.nl>
2024-11-19 14:18:11 +0100

    use nested namespace

--- sources/Deltares.Probabilistic.Test/Math/testRootfinder_body.cpp
+++ sources/Deltares.Probabilistic.Test/Math/testRootfinder_body.cpp
@@ -26,102 +26,94 @@
 #include "../../Deltares.Probabilistic/Math/RootFinders/LinearRootFinder.h"
 #include "../../Deltares.Probabilistic/Utils/probLibException.h"
 
-using namespace Deltares::Numeric;
-
-namespace Deltares
+namespace Deltares::Probabilistic::Test
 {
-    namespace Probabilistic
+
+    void rootfinder_tests::allRootfinder_tests()
     {
-        namespace Test
-        {
-
-            void rootfinder_tests::allRootfinder_tests()
-            {
-                bisection_test();
-                bisection_test2();
-                bisection_const_test();
-                linear_root_finder_test();
-                linear_root_finder_test2();
-                linear_root_finder_const_test();
-            }
-
-            double rootfinder_tests::testLinearFunc(double x) const
-            {
-                return a + b * x;
-            }
-
-            double rootfinder_tests::testConstFunc(double x)
-            {
-                cnt++;
-                if (cnt > 1000) throw Reliability::probLibException("too many function calls");
-                return 1.0;
-            }
-
-            void rootfinder_tests::bisection_test()
-            {
-                a = 3.0;
-                b = 0.5;
-                auto bisectionRF = BisectionRootFinder();
-                auto result = bisectionRF.CalculateValue(1.0, 2.0, 0.0,
-                    [this](double x) {return testLinearFunc(x); });
-                ASSERT_NEAR(result, -6.0, 1e-12);
-            }
-
-            void rootfinder_tests::bisection_test2()
-            {
-                a = 3.0;
-                b = 0.5;
-                auto bisectionRF = BisectionRootFinder();
-                auto result = bisectionRF.CalculateValue(-1.0, -2.0, 0.0,
-                    [this](double x) {return testLinearFunc(x); });
-                ASSERT_NEAR(result, -6.0, 1e-12);
-            }
-
-            void rootfinder_tests::bisection_const_test()
-            {
-                cnt = 0;
-                auto bisectionRF = BisectionRootFinder();
-                EXPECT_NO_THROW(
-                    {
-                        auto result = bisectionRF.CalculateValue(-1.0, -2.0, 0.0,
-                            [this](double x) {return testConstFunc(x); });
-                        ASSERT_TRUE(std::isnan(result));
-                    });
-            }
-
-            void rootfinder_tests::linear_root_finder_test()
-            {
-                a = 3.0;
-                b = 0.5;
-                auto linear_root_finder = LinearRootFinder(1e-6, 25);
-                auto result = linear_root_finder.CalculateValue(1.0, 2.0, 0.0,
-                    [this](double x) {return testLinearFunc(x); });
-                ASSERT_NEAR(result, -6.0, 1e-12);
-            }
-
-            void rootfinder_tests::linear_root_finder_test2()
-            {
-                a = 3.0;
-                b = 0.5;
-                auto linear_root_finder = LinearRootFinder(1e-6, 25);
-                auto result = linear_root_finder.CalculateValue(-1.0, -2.0, 0.0,
-                    [this](double x) {return testLinearFunc(x); });
-                ASSERT_NEAR(result, -6.0, 1e-12);
-            }
-
-            void rootfinder_tests::linear_root_finder_const_test()
-            {
-                cnt = 0;
-                auto linear_root_finder = LinearRootFinder(1e-6, 25);
-                EXPECT_NO_THROW(
-                    {
-                        auto result = linear_root_finder.CalculateValue(-1.0, -2.0, 0.0,
-                            [this](double x) {return testConstFunc(x); });
-                        ASSERT_TRUE(std::isnan(result));
-                    });
-            }
-
-        }
+        bisection_test();
+        bisection_test2();
+        bisection_const_test();
+        linear_root_finder_test();
+        linear_root_finder_test2();
+        linear_root_finder_const_test();
     }
+
+    double rootfinder_tests::testLinearFunc(double x) const
+    {
+        return a + b * x;
+    }
+
+    double rootfinder_tests::testConstFunc(double x)
+    {
+        cnt++;
+        if (cnt > 1000) throw Reliability::probLibException("too many function calls");
+        return 1.0;
+    }
+
+    void rootfinder_tests::bisection_test()
+    {
+        a = 3.0;
+        b = 0.5;
+        auto bisectionRF = Numeric::BisectionRootFinder();
+        auto result = bisectionRF.CalculateValue(1.0, 2.0, 0.0,
+            [this](double x) {return testLinearFunc(x); });
+        ASSERT_NEAR(result, -6.0, 1e-12);
+    }
+
+    void rootfinder_tests::bisection_test2()
+    {
+        a = 3.0;
+        b = 0.5;
+        auto bisectionRF = Numeric::BisectionRootFinder();
+        auto result = bisectionRF.CalculateValue(-1.0, -2.0, 0.0,
+            [this](double x) {return testLinearFunc(x); });
+        ASSERT_NEAR(result, -6.0, 1e-12);
+    }
+
+    void rootfinder_tests::bisection_const_test()
+    {
+        cnt = 0;
+        auto bisectionRF = Numeric::BisectionRootFinder();
+        EXPECT_NO_THROW(
+            {
+                auto result = bisectionRF.CalculateValue(-1.0, -2.0, 0.0,
+                    [this](double x) {return testConstFunc(x); });
+                ASSERT_TRUE(std::isnan(result));
+            });
+    }
+
+    void rootfinder_tests::linear_root_finder_test()
+    {
+        a = 3.0;
+        b = 0.5;
+        auto linear_root_finder = Numeric::LinearRootFinder(1e-6, 25);
+        auto result = linear_root_finder.CalculateValue(1.0, 2.0, 0.0,
+            [this](double x) {return testLinearFunc(x); });
+        ASSERT_NEAR(result, -6.0, 1e-12);
+    }
+
+    void rootfinder_tests::linear_root_finder_test2()
+    {
+        a = 3.0;
+        b = 0.5;
+        auto linear_root_finder = Numeric::LinearRootFinder(1e-6, 25);
+        auto result = linear_root_finder.CalculateValue(-1.0, -2.0, 0.0,
+            [this](double x) {return testLinearFunc(x); });
+        ASSERT_NEAR(result, -6.0, 1e-12);
+    }
+
+    void rootfinder_tests::linear_root_finder_const_test()
+    {
+        cnt = 0;
+        auto linear_root_finder = Numeric::LinearRootFinder(1e-6, 25);
+        EXPECT_NO_THROW(
+            {
+                auto result = linear_root_finder.CalculateValue(-1.0, -2.0, 0.0,
+                    [this](double x) {return testConstFunc(x); });
+                ASSERT_TRUE(std::isnan(result));
+            });
+    }
+
 }
 
