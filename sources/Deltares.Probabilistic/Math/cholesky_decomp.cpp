f3bd32c669a3b9d12ed14704f7da9d7b6d05cd09
Edwin Spee <edwin.spee@deltares.nl>
2024-11-20 16:13:21 +0100

    revert removal of include file

--- sources/Deltares.Probabilistic/Math/cholesky_decomp.cpp
+++ sources/Deltares.Probabilistic/Math/cholesky_decomp.cpp
@@ -19,6 +19,7 @@
 // Stichting Deltares and remain full property of Stichting Deltares at all times.
 // All rights reserved.
 //
+#include <cmath>
 #include "matrix.h"
 #include "../Utils/probLibException.h"
 
