14f57be8802c5f82e9fe90791861761bf4be4861
Edwin Spee <edwin.spee@deltares.nl>
2024-12-09 14:04:45 +0100

    run scripts on all sources

--- sources/Deltares.Probabilistic/Reliability/StartPointCalculatorSettings.cpp
+++ sources/Deltares.Probabilistic/Reliability/StartPointCalculatorSettings.cpp
@@ -1,8 +1,8 @@
 // Copyright (C) Stichting Deltares. All rights reserved.
 //
-// This file is part of Streams.
+// This file is part of the Probabilistic Library.
 //
-// Streams is free software: you can redistribute it and/or modify
+// The Probabilistic Library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
