6c13d40cc4deff6dea5fdf02c970db260b9a2e00
Edwin Spee <34799234+epsig@users.noreply.github.com>
2024-12-13 10:38:06 +0100

    Feature/probl 112 update license (#85)
    * update template header file
    
    * update header in extra folder (mainly by hand)
    
    * update header with remove and add script in source folder
    
    * manual update of header in TestDistributions (also streams => prob. lib.)
    
    * update top level license file
    
    * found three more occurences of AGPL

--- sources/Deltares.Probabilistic.PWrapper/setup.py
+++ sources/Deltares.Probabilistic.PWrapper/setup.py
@@ -3,16 +3,16 @@
 # This file is part of the Probabilistic Library.
 #
 # The Probabilistic Library is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
+# it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
+# GNU Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU Affero General Public License
+# You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 # All names, logos, and references to "Deltares" are registered trademarks of
@@ -36,7 +36,7 @@ setuptools.setup(
     package_data={'probabilistic_library':['bin/*Deltares.Probabilistic.*.*']},    
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: AGPL License",
+        "License :: OSI Approved :: LGPL License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
