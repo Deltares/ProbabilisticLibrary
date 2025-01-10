d69199a19aee8c069176560a1d19c304de1ef6db
Edwin Spee <edwin.spee@deltares.nl>
2025-01-10 16:27:29 +0100

    remove extra sample from KMeansClustering

--- sources/Deltares.Probabilistic/Optimization/KMeansClustering.cpp
+++ sources/Deltares.Probabilistic/Optimization/KMeansClustering.cpp
@@ -217,9 +217,6 @@ namespace Deltares
             // select one data item index at random as 1st mean
             random.initialize(Settings->generatorType, true, randomSeed);
 
-            // TODO: PROBL-42 remove this line 
-            double dummy = random.next();  //  random->Next(0, samples.size()); // [0, N)
-
             auto firstCluster = std::make_shared<Cluster>();
 
             firstCluster->Samples.reserve(samples.size());
