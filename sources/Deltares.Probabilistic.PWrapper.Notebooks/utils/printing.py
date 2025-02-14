8a49331de37c400eaa7a794848f6d213358d5668
Karolina Wojciechowska <karolina.wojciechowska@deltares.nl>
2025-02-14 16:17:31 +0100

    improvements

--- sources/Deltares.Probabilistic.PWrapper.Notebooks/utils/printing.py
+++ sources/Deltares.Probabilistic.PWrapper.Notebooks/utils/printing.py
@@ -1,9 +1,20 @@
 
 from probabilistic_library import StandardNormal, ReliabilityMethod
 
-# print design point
-def print_results(dp, project):
+# print convergence information and model runs
+def print_conv(dp, project):
 
+    if dp.is_converged:
+        print(f"Converged (convergence = {dp.convergence} < {project.settings.variation_coefficient})")
+    else:
+        print(f"Not converged (convergence = {dp.convergence} > {project.settings.variation_coefficient})")
+    
+    print(f"Model runs = {dp.total_model_runs}")
+
+# print design point
+def print_results(project):
+
+    dp = project.design_point
     beta = dp.reliability_index
 
     print(f"Beta = {beta}")
@@ -14,25 +25,12 @@ def print_results(dp, project):
     for alpha in dp.alphas:
         print(f"{alpha.variable.name}: alpha = {alpha.alpha}, x = {alpha.x}")
 
-    if dp.is_converged:
-        print(f"Converged (convergence = {dp.convergence} < {project.settings.variation_coefficient})")
-    else:
-        print(f"Not converged (convergence = {dp.convergence} > {project.settings.variation_coefficient})")
-        
-    print(f"Model runs = {dp.total_model_runs}")
-
-# print convergence information and model runs
-def print_statistics(dp, project):
-    if dp.is_converged:
-        print(f"Converged (convergence = {dp.convergence} < {project.settings.variation_coefficient})")
-    else:
-        print(f"Not converged (convergence = {dp.convergence} > {project.settings.variation_coefficient})")
-    
-    print(f"Model runs = {dp.total_model_runs}")
+    print_conv(dp, project)
 
 # print for hybrid methods
-def print_hybrid(dp, project):
+def print_hybrid(project):
 
+    dp = project.design_point
     beta = dp.reliability_index
 
     print(f"Beta = {beta}")
@@ -45,20 +43,20 @@ def print_hybrid(dp, project):
 
     if project.settings.reliability_method == ReliabilityMethod.directional_sampling_then_form:
         print("Direction sampling step:")
-        print_statistics(dp.contributing_design_points[0], project)
+        print_conv(dp.contributing_design_points[0], project)
         print("FORM step:")
-        print_statistics(dp.contributing_design_points[1], project)
+        print_conv(dp.contributing_design_points[1], project)
     else:
-        print_statistics(dp, project)
+        print_conv(dp, project)
 
 # print input and output variables
 def print_input_output_var(project):
 
     print("Input parameters:")
     for input_parameter in project.model.input_parameters:
-        print (input_parameter)
+        print(input_parameter)
             
     print("")
     print("Output parameters:")
     for output_parameter in project.model.output_parameters:
-        print (output_parameter)
+        print(output_parameter)
