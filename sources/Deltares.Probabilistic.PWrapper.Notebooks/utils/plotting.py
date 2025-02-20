f48434259835ca9a200287bdabf46a573d71ccb7
Karolina Wojciechowska <karolina.wojciechowska@deltares.nl>
2025-02-20 11:23:15 +0100

    added visualization notebook

--- sources/Deltares.Probabilistic.PWrapper.Notebooks/utils/plotting.py
+++ sources/Deltares.Probabilistic.PWrapper.Notebooks/utils/plotting.py
@@ -21,56 +21,56 @@ def plot_dist(val_grid, stochast):
     ax2.plot(val_grid, cdf, "r--", label="pdf")
     ax2.tick_params(axis="y", labelcolor=color)
 
-# plot piechart of alpha values
-def piechart_alphas(project):
+# plot pie chart of alpha values
+def piechart_alphas(dp, pm):
 
-    alphas = [project.design_point.alphas[val.name].alpha**2 for val in project.model.input_parameters]
-    names = [val.name for val in project.model.input_parameters]
+    alphas = [dp.alphas[val.name].alpha**2 for val in pm.input_parameters]
+    names = [f"{val.name} ({round(100*alphas[id], 1)})" if alphas[id]*100>0.1 else "" for id, val in enumerate(pm.input_parameters)]
     
     plt.figure()
-    plt.pie(alphas, labels=names, autopct='%1.2f%%')
+    plt.pie(alphas, labels=names)
     plt.title("alpha^2", fontsize=14, fontweight='bold')
 
 # plot realizations when at least 2 random variables
-def plot_realizations_2d(project):
+def plot_realizations_2d(dp, pm):
 
-    n = len(project.design_point.realizations)
-    z = [project.design_point.realizations[id].z for id in range(n)]
+    n = len(dp.realizations)
+    z = [dp.realizations[id].z for id in range(n)]
 
     # 2 variables with the highest alpha
-    alphas = [project.design_point.alphas[val.name].alpha for val in project.model.input_parameters]
+    alphas = [dp.alphas[val.name].alpha for val in pm.input_parameters]
     index_last_two = np.argsort(np.abs(alphas))[-2:]
 
-    r_1 = [realization.input_values[int(index_last_two[0])] for realization in project.design_point.realizations]
-    r_2 = [realization.input_values[int(index_last_two[1])] for realization in project.design_point.realizations]
+    r_1 = [realization.input_values[int(index_last_two[0])] for realization in dp.realizations]
+    r_2 = [realization.input_values[int(index_last_two[1])] for realization in dp.realizations]
     colors = ["r" if val < 0 else "g" for val in z]
 
     # plot realizations
     plt.figure()
     plt.grid(True)    
     plt.scatter(r_1, r_2, color=colors, alpha=0.5)
-    plt.scatter(project.design_point.alphas[int(index_last_two[0])].x, 
-                project.design_point.alphas[int(index_last_two[1])].x, 
+    plt.scatter(dp.alphas[int(index_last_two[0])].x, 
+                dp.alphas[int(index_last_two[1])].x, 
                 label="design point", 
                 color="black")
-    plt.xlabel(project.model.input_parameters[int(index_last_two[0])].name)
-    plt.ylabel(project.model.input_parameters[int(index_last_two[1])].name)
+    plt.xlabel(pm.input_parameters[int(index_last_two[0])].name)
+    plt.ylabel(pm.input_parameters[int(index_last_two[1])].name)
     plt.legend()
     plt.title("Realizations: Red = Failure, Green = No Failure", fontsize=14, fontweight='bold')
 
 # plot realizations when at least 3 random variables
-def plot_realizations_3d(project):
+def plot_realizations_3d(dp, pm):
 
-    n = len(project.design_point.realizations)
-    z = [project.design_point.realizations[id].z for id in range(n)]
+    n = len(dp.realizations)
+    z = [dp.realizations[id].z for id in range(n)]
 
     # 3 variables with the highest alpha
-    alphas = [project.design_point.alphas[val.name].alpha for val in project.model.input_parameters]
+    alphas = [dp.alphas[val.name].alpha for val in pm.input_parameters]
     index_last_three = np.argsort(np.abs(alphas))[-3:]
 
-    r_1 = [realization.input_values[int(index_last_three[0])] for realization in project.design_point.realizations]
-    r_2 = [realization.input_values[int(index_last_three[1])] for realization in project.design_point.realizations]
-    r_3 = [realization.input_values[int(index_last_three[2])] for realization in project.design_point.realizations]
+    r_1 = [realization.input_values[int(index_last_three[0])] for realization in dp.realizations]
+    r_2 = [realization.input_values[int(index_last_three[1])] for realization in dp.realizations]
+    r_3 = [realization.input_values[int(index_last_three[2])] for realization in dp.realizations]
     colors = ["r" if val < 0 else "g" for val in z]
 
     # plot realizations
@@ -78,23 +78,23 @@ def plot_realizations_3d(project):
     ax = fig.add_subplot(projection='3d')
     ax.grid(True)    
     ax.scatter(r_1, r_2, color=colors, alpha=0.5)
-    ax.scatter(project.design_point.alphas[int(index_last_three[0])].x, 
-                project.design_point.alphas[int(index_last_three[1])].x, 
-                project.design_point.alphas[int(index_last_three[2])].x,
+    ax.scatter(dp.alphas[int(index_last_three[0])].x, 
+                dp.alphas[int(index_last_three[1])].x, 
+                dp.alphas[int(index_last_three[2])].x,
                 label="design point", 
                 color="black")
-    ax.set_xlabel(project.model.input_parameters[int(index_last_three[0])].name)
-    ax.set_ylabel(project.model.input_parameters[int(index_last_three[1])].name)
-    ax.set_zlabel(project.model.input_parameters[int(index_last_three[2])].name)
+    ax.set_xlabel(pm.input_parameters[int(index_last_three[0])].name)
+    ax.set_ylabel(pm.input_parameters[int(index_last_three[1])].name)
+    ax.set_zlabel(pm.input_parameters[int(index_last_three[2])].name)
     #ax.set_legend()
     ax.set_title("Realizations: Red = Failure, Green = No Failure", fontsize=14, fontweight='bold')
 
 # plot beta and failure probability
-def plot_beta_prob(project):
+def plot_beta_prob(dp):
 
-    n = len(project.design_point.realizations)
-    z = [project.design_point.realizations[id].z for id in range(n)]
-    beta = [project.design_point.realizations[id].beta for id in range(n)]
+    n = len(dp.realizations)
+    z = [dp.realizations[id].z for id in range(n)]
+    beta = [dp.realizations[id].beta for id in range(n)]
 
     prob = [1-StandardNormal.get_p_from_u(val) for val in beta]
 
@@ -106,7 +106,7 @@ def plot_beta_prob(project):
     plt.title("beta values", fontsize=14, fontweight='bold')
 
     plt.figure()
-    plt.plot(prob)
+    plt.semilogy(prob)
     plt.xlabel("model run (-)")
     plt.ylabel("failure probability (-)")
     plt.grid()
@@ -155,9 +155,9 @@ def find_Z_0(project, f, given_var, given_val, find_var, start_int, end_int, tol
 
     return (start_int + end_int) / 2
 
-def plot_Z_0(project, f, given_var, given_val_range, find_var, start_int, end_int):
+def plot_Z_0(project, f, given_var, given_var_range, find_var, start_int, end_int):
 
-    find_val = [find_Z_0(project, f, given_var, val, find_var, start_int, end_int, tol=1e-5, max_iter=100) for val in given_val_range]
+    find_val = [find_Z_0(project, f, given_var, val, find_var, start_int, end_int, tol=1e-5, max_iter=100) for val in given_var_range]
 
     x_given_var = project.design_point.alphas[given_var].x
     x_find_var = project.design_point.alphas[find_var].x
@@ -168,7 +168,7 @@ def plot_Z_0(project, f, given_var, given_val_range, find_var, start_int, end_in
             other_dp.append(f"{key.name} = {round(project.design_point.alphas[key.name].x, 2)}")
 
     plt.figure()
-    plt.plot(given_val_range, find_val, label="Z=0")
+    plt.plot(given_var_range, find_val, label="Z=0")
     plt.scatter(x_given_var, x_find_var, label="design point", color="black")
     plt.xlabel(given_var)
     plt.ylabel(find_var)
