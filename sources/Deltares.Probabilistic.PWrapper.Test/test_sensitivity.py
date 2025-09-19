cdd3b13c9f0eeb93e495ac03bf889cdb4a6523e5
Rob Brinkman <rob.brinkman@deltares.nl>
2025-09-19 13:47:50 +0200

    added sobol tests

--- sources/Deltares.Probabilistic.PWrapper.Test/test_sensitivity.py
+++ sources/Deltares.Probabilistic.PWrapper.Test/test_sensitivity.py
@@ -58,10 +58,14 @@ class Test_sensitivity(unittest.TestCase):
         sens1 = project.results[0]
 
         self.assertEqual('x' , sens1.identifier)
+
+        self.assertEqual(sens1 , project.results['x'])
+
         self.assertEqual(2, len(sens1.values))
         self.assertAlmostEqual(2.07, sens1.values[0].low, delta=margin)
         self.assertAlmostEqual(1.8, sens1.values[0].medium, delta=margin)
         self.assertAlmostEqual(1.53, sens1.values[0].high, delta=margin)
+        self.assertEqual(project.variables[0].name , sens1.values[0].variable.name) # should be equal without name
 
         self.assertAlmostEqual(3.51, sens1.values[1].low, delta=margin)
         self.assertAlmostEqual(1.8, sens1.values[1].medium, delta=margin)
@@ -74,6 +78,48 @@ class Test_sensitivity(unittest.TestCase):
         self.assertAlmostEqual(2.34, sens2.values[0].low, delta=margin)
         self.assertAlmostEqual(1.8, sens2.values[0].medium, delta=margin)
         self.assertAlmostEqual(1.26, sens2.values[0].high, delta=margin)
+
+    def test_sobol_linear(self):
+        project = project_builder.get_sensitivity_linear_project()
+
+        project.settings.sensitivity_method = SensitivityMethod.sobol
+
+        project.run()
+
+        sens = project.result
+
+        self.assertEqual('L - (a+b)' , sens.identifier)
+        self.assertEqual(2, len(sens.values))
+        self.assertAlmostEqual(0.501, sens.values[0].first_order_index, delta=margin)
+        self.assertAlmostEqual(0.504, sens.values[0].total_index, delta=margin)
+        self.assertAlmostEqual(0.499, sens.values[1].first_order_index, delta=margin)
+        self.assertAlmostEqual(0.496, sens.values[1].total_index, delta=margin)
+
+    def test_sobol_multiple_unbalanced_linear(self):
+        project = project_builder.get_sensitivity_multiple_unbalanced_linear_project()
+
+        project.settings.sensitivity_method = SensitivityMethod.sobol
+
+        project.run();
+
+        self.assertEqual(3, len(project.results))
+
+        sens1 = project.results[0]
+
+        self.assertEqual('x' , sens1.identifier)
+        self.assertEqual(2, len(sens1.values))
+        self.assertAlmostEqual(0.02, sens1.values[0].first_order_index, delta=margin)
+        self.assertAlmostEqual(0.02, sens1.values[0].total_index, delta=margin)
+
+        self.assertAlmostEqual(0.98, sens1.values[1].first_order_index, delta=margin)
+        self.assertAlmostEqual(0.98, sens1.values[1].total_index, delta=margin)
+
+        sens2= project.results[1]
+
+        self.assertEqual('y' , sens2.identifier)
+        self.assertEqual(2, len(sens2.values))
+        self.assertAlmostEqual(0.175, sens2.values[0].first_order_index, delta=margin)
+        self.assertAlmostEqual(0.175, sens2.values[0].total_index, delta=margin)
 
 if __name__ == '__main__':
     unittest.main()
