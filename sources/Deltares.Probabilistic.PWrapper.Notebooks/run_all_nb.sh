e7a246669d066220ef85f8c407d3209bbc2d2422
Edwin Spee <edwin.spee@deltares.nl>
2025-12-04 14:30:31 +0100

    run all notebooks

--- sources/Deltares.Probabilistic.PWrapper.Notebooks/run_all_nb.sh
+++ sources/Deltares.Probabilistic.PWrapper.Notebooks/run_all_nb.sh
@@ -2,10 +2,6 @@
 
 set -e
 for nb in *.ipynb; do
-	case $nb in
-		safety_levels.ipynb) continue;;
-	esac
-
 	echo "##teamcity[testStarted name='$nb']"
 	jupyter-nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute $nb
 	echo "##teamcity[testFinished name='$nb']"
