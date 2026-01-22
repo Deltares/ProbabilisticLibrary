c442f62a1ddab9e00c218fe57ae2d9b937dfcee8
Edwin Spee <edwin.spee@deltares.nl>
2026-01-22 15:41:11 +0100

    clean up: containers, examples and presentations
    to new repos: Probabilistic-Suite-Data

--- sources/Deltares.Probabilistic.PWrapper.Examples/kratos/wheels/install.bat
+++
@@ -1,6 +0,0 @@
-"@echo off" 
-rem Execute this file in this directory with the wheel files provided 
-python -m pip install --find-links=. KratosMultiphysics==9.5.0.5 --force-reinstall
-python -m pip install --find-links=. KratosStructuralMechanicsApplication==9.5.0.5 --force-reinstall
-python -m pip install --find-links=. KratosGeoMechanicsApplication==9.5.0.5 --force-reinstall
-
--- sources/containers/problibbuild/build_cmake.sh
+++
@@ -1,9 +0,0 @@
-version=3.28
-build=3
-## don't modify from here
-limit=3.20
-os=linux
-wget https://cmake.org/files/v$version/cmake-$version.$build-$os-x86_64.sh 
-mkdir /opt/cmake
-sh cmake-$version.$build-$os-x86_64.sh --prefix=/opt/cmake --skip-license
-rm cmake-$version.$build-$os-x86_64.sh
