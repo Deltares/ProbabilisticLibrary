6abf397142cc9c6cb7c7e1ec51dcd5b4eaf6b07d
Edwin Spee <34799234+epsig@users.noreply.github.com>
2026-01-23 14:53:56 +0100

    Release/26.1.1 (#288)
    * update version number to 26.1.1
    * update link to scientific documentation

--- sources/Deltares.Probabilistic.PWrapper/update_wheel.bat
+++ sources/Deltares.Probabilistic.PWrapper/update_wheel.bat
@@ -1,2 +1,2 @@
 python setup.py bdist_wheel
-pip install dist/probabilistic_library-25.3.1-py3-none-any.whl --force-reinstall
+pip install dist/probabilistic_library-26.1.1-py3-none-any.whl --force-reinstall
