fca758633bc7d8ded549205af8e5e451553366ba
hauth <matthias.hauth@deltares.nl>
2026-05-28 11:52:39 +0200

    Skip notebooks with Dsheet in bash file nb

--- web/notebooks/run_all_nb.sh
+++ web/notebooks/run_all_nb.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
 set -e
+
+# Notebook exceptions: add filenames here to skip them in CI.
+# Example:
+#   SKIP_NOTEBOOKS=(
+#     "Case_2_DSheetPiling_Tutorial.ipynb"
+#   )
+SKIP_NOTEBOOKS=(
+	"Case_2_DSheetPiling_Tutorial.ipynb"
+	"Case_3_DSheetPiling_FragilityCurve.ipynb"
+)
+
+should_skip() {
+    local nb="$1"
+    for skip_nb in "${SKIP_NOTEBOOKS[@]}"; do
+        if [ "$nb" = "$skip_nb" ]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
 for nb in *.ipynb; do
+	if should_skip "$nb"; then
+		echo "Skipping notebook (exception list): $nb"
+		continue
+	fi
+
 	echo "##teamcity[testStarted name='$nb']"
 	jupyter-nbconvert --to notebook --ExecutePreprocessor.timeout=-1 --execute $nb
 	echo "##teamcity[testFinished name='$nb']"
