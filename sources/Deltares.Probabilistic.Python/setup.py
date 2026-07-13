d8d9aa8511f67fc83c7ae421923b03333fa1a2d2
Edwin Spee <edwin.spee@deltares.nl>
2026-07-13 09:37:23 +0200

    remove rc from version numbers

--- sources/Deltares.Probabilistic.Python/setup.py
+++ sources/Deltares.Probabilistic.Python/setup.py
@@ -23,7 +23,7 @@ import setuptools
 
 setuptools.setup(
     name="probabilistic_library",
-    version="26.2.1-rc2",
+    version="26.2.1",
     author="Deltares",
     author_email="d-prob-support@deltares.nl",
     description="Package which provides probabilistic methods",
