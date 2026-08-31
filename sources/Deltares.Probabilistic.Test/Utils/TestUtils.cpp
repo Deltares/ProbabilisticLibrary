469b0f53d94facca07656b5628e69414d95d9e53
Edwin Spee <edwin.spee@deltares.nl>
2026-08-31 11:18:27 +0200

    rename to Pascal Case in utils

--- sources/Deltares.Probabilistic.Test/Utils/TestUtils.cpp
+++ sources/Deltares.Probabilistic.Test/Utils/TestUtils.cpp
@@ -19,7 +19,7 @@
 // Stichting Deltares and remain full property of Stichting Deltares at all times.
 // All rights reserved.
 //
-#include "testutils.h"
+#include "TestUtils.h"
 #include <gtest/gtest.h>
 #include <cmath>
 #include <fstream>
